f34310b9f04f0a1483ae3e217f7db995efa067a8
Ferdi ÜNAL <ferdiunal@outlook.com>
2026-05-23 21:19:23 +0300

    ci: stabilize prefer-lowest quality gates

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,6 +1,5 @@
 <?php
 
-it('will not use debugging functions')
-    // @phpstan-ignore-next-line Pest architecture expectations are registered at runtime.
-    ->expect(['dd', 'dump', 'ray'])
-    ->not->toBeUsed();
+it('will not use debugging functions', function (): void {
+    expect(['dd', 'dump', 'ray'])->not->toBeUsed();
+});
